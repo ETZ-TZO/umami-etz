a7f4b8bd0c626b2f60f2463a6e5e95560d6ddc77
Sam Course <notcourseps@gmail.com>
2022-06-29 13:58:26 +0200

    Added possibility to export analytics data with a button
    The shadow button is a hacky way of doing it but there unfortunately didn't seem to be any other way

--- components/metrics/WebsiteHeader.js
+++ components/metrics/WebsiteHeader.js
@@ -10,8 +10,34 @@ import Favicon from 'components/common/Favicon';
 import ActiveUsers from './ActiveUsers';
 import Arrow from 'assets/arrow-right.svg';
 import styles from './WebsiteHeader.module.css';
+import useFetch from 'hooks/useFetch';
+import Button from 'components/common/Button';
+import { getItem } from '../../lib/web';
+import { AUTH_TOKEN } from '../../lib/constants';
 
 export default function WebsiteHeader({ websiteId, title, domain, showLink = false }) {
+  const { data } = useFetch(`/website/${websiteId}/events`, {
+    params: {
+      token: getItem(AUTH_TOKEN),
+      download: true,
+    },
+  });
+
+  function downloadAsCSV() {
+    let csv = objectToCSV(data);
+
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+    const ExportToCsvButton = document.createElement('a');
+    ExportToCsvButton.setAttribute('hidden', '');
+    ExportToCsvButton.setAttribute('href', url);
+    ExportToCsvButton.setAttribute('download', 'analytics.csv');
+
+    document.body.appendChild(ExportToCsvButton);
+    ExportToCsvButton.click();
+    document.body.removeChild(ExportToCsvButton);
+  }
+
   const header = showLink ? (
     <>
       <Favicon domain={domain} />
@@ -36,6 +62,11 @@ export default function WebsiteHeader({ websiteId, title, domain, showLink = fal
       <div className={classNames(styles.active, 'col-12 col-lg-4 order-3 order-lg-2')}>
         <ActiveUsers websiteId={websiteId} />
       </div>
+      <ButtonLayout align="center">
+        <Button className={styles.button} type={'button'} onClick={downloadAsCSV}>
+          Export data to CSV
+        </Button>
+      </ButtonLayout>
       <div className="col-2 col-lg-4 order-2 order-lg-3">
         <ButtonLayout align="right">
           <RefreshButton websiteId={websiteId} />
@@ -55,4 +86,20 @@ export default function WebsiteHeader({ websiteId, title, domain, showLink = fal
       </div>
     </PageHeader>
   );
+}
+
+function objectToCSV(data) {
+  const csvRows = [];
+
+  const headers = Object.keys(data[0]);
+  csvRows.push(headers.join(','));
+
+  for (const row of data) {
+    const values = headers.map(header => {
+      const escaped = ('' + row[header]).replace(/"/g, '\\"');
+      return `"${escaped}"`;
+    });
+    csvRows.push(values.join(','));
+  }
+  return csvRows.join('\n');
 }
