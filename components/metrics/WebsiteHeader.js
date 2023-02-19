5fa6d33dfa7a85a015b1f8b4e7fe485557de88b9
Sam Course <notcourseps@gmail.com>
2023-02-19 14:29:35 +0100

    Fix data export

--- components/metrics/WebsiteHeader.js
+++ components/metrics/WebsiteHeader.js
@@ -56,3 +56,31 @@ export default function WebsiteHeader({ websiteId, title, domain, showLink = fal
     </PageHeader>
   );
 }
+
+function objectToCSV(data) {
+  data = sortData(data);
+
+  const csvRows = [];
+
+  const headers = Object.keys(data[0]);
+  csvRows.push(headers.join(','));
+
+  for (const row of data) {
+    const values = headers.map(header => {
+      const escaped = ('' + row[header]).replace(/"/g, '\\"');
+      return `"${escaped}"`;
+    });
+    csvRows.push(values.join(','));
+  }
+  return csvRows.join('\n');
+}
+
+/**
+ * Sort the passed data by patient id (url) and then by date
+ * @param data The data that needs to be sorted
+ * @returns {*} The sorted data
+ */
+function sortData(data) {
+  data.sort((a, b) => (a.url > b.url ? 1 : a.url === b.url ? (a.date > b.date ? -1 : 1) : -1));
+  return data;
+}
